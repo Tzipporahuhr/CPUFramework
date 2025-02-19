d8795897772dda32834fe6edd4ddb432268b68c3
Tzipporahuhr <tzipporahuhr@gmail.com>
2025-02-19 14:28:30 -0500

    issue

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -7,7 +7,7 @@ namespace CPUFramework
     public class SQLUtility
     {   public static string ConnectionString= "";
         public static DataTable GetDataTable(string sqlstatement)
-        {  Debug.Print(sqlstatement);
+        {   Debug.Print(sqlstatement);
             DataTable dt = new();
             SqlConnection conn = new();
             conn.ConnectionString = ConnectionString;
@@ -18,7 +18,28 @@ namespace CPUFramework
             cmd.CommandText = sqlstatement; ;
             var dr = cmd.ExecuteReader();
             dt.Load(dr);
+           SetAllColumnsAllowNull(dt);
+            
             return dt;
+        }
+
+        private static void SetAllColumnsAllowNull(DataTable dt)
+        {
+        foreach(DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull= true;
+            }
+        }
+
+        public static void DebugPrintDataTable(DataTable dt)
+        {
+            foreach(DataRow r in dt.Rows)
+            {
+                foreach(DataColumn c in dt.Columns)
+                {
+                    Debug.Print(c.ColumnName + "=" + r[c.ColumnName].ToString());
+                }
+            }
         }
 
     }
