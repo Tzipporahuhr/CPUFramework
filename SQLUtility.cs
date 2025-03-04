93532802beec2bf8f7b040aefd443559f2292146
Tzipporahuhr <tzipporahuhr@gmail.com>
2025-03-04 14:15:26 -0500

    done

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -5,22 +5,63 @@ using System.Diagnostics;
 namespace CPUFramework
 {
     public class SQLUtility
-    {   public static string ConnectionString= "";
+    {
+        public static string ConnectionString = "";
+
         public static DataTable GetDataTable(string sqlstatement)
-        {  Debug.Print(sqlstatement);
+        {
+            Debug.Print(sqlstatement);
             DataTable dt = new();
             SqlConnection conn = new();
             conn.ConnectionString = ConnectionString;
             conn.Open();
-
             var cmd = new SqlCommand();
             cmd.Connection = conn;
-            cmd.CommandText = sqlstatement; ;
+            cmd.CommandText = sqlstatement;
             var dr = cmd.ExecuteReader();
             dt.Load(dr);
+            SetAllColumnsAllowNull(dt);
             return dt;
+        }
+
+        public static void ExecuteSQL(string sqlstatment)
+        {
+            GetDataTable(sqlstatment);
+        }
+
+        public static int GetFirstColumnFirstRowValue(string sql)
+        {
+            int n = 0;
+
+            DataTable dt = GetDataTable(sql);
+            if (dt.Rows.Count > 0 && dt.Columns.Count > 0)
+            {
+                if (dt.Rows[0][0] != DBNull.Value)
+                {
+                    int.TryParse(dt.Rows[0][0].ToString(), out n);
+                }
+            }
+            return n;
+        }
+
+        private static void SetAllColumnsAllowNull(DataTable dt)
+        {
+            foreach (DataColumn c in dt.Columns)
+            {
+                c.AllowDBNull = true;
+            }
+        }
+
+        public static void DebugPrintDataTable(DataTable dt)
+        {
+            foreach (DataRow r in dt.Rows)
+            {
+                foreach (DataColumn c in dt.Columns)
+                {
+                    Debug.Print(c.ColumnName + " = " + r[c.ColumnName].ToString());
+                }
+            }
         }
 
     }
 }
- 
