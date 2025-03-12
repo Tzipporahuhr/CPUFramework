9a53474c0eea4ff233dcda026fa2bc83c704584f
Tzipporahuhr <tzipporahuhr@gmail.com>
2025-03-11 22:49:55 -0400

    15 in progress

--- SQLUtility.cs
+++ SQLUtility.cs
@@ -8,20 +8,40 @@ namespace CPUFramework
     {
         public static string ConnectionString = "";
 
+        public static SqlCommand GetSqlCommand(string sprocname)
+        {
+            SqlCommand cmd;
+            using (SqlConnection conn = new SqlConnection(SQLUtility.ConnectionString))
+            {
+               cmd = new SqlCommand(sprocname, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                conn.Open();
+                SqlCommandBuilder.DeriveParameters(cmd);
+            }
+            return cmd;
+        }
+
+        public static DataTable GetDataTable(SqlCommand cmd)
+        {
+            Debug.Print("----------" + Environment.NewLine+cmd.CommandText);
+            DataTable dt = new();
+            using(SqlConnection conn= new SqlConnection(SQLUtility.ConnectionString))
+            {
+                conn.Open();
+                cmd.Connection= conn;
+                SqlDataReader dr= cmd.ExecuteReader();
+                dt.Load(dr);
+
+            }
+            SetAllColumnsAllowNull(dt);
+            return dt;
+        }
+
         public static DataTable GetDataTable(string sqlstatement)
         {
             Debug.Print(sqlstatement);
-            DataTable dt = new();
-            SqlConnection conn = new();
-            conn.ConnectionString = ConnectionString;
-            conn.Open();
-            var cmd = new SqlCommand();
-            cmd.Connection = conn;
-            cmd.CommandText = sqlstatement;
-            var dr = cmd.ExecuteReader();
-            dt.Load(dr);
-            SetAllColumnsAllowNull(dt);
-            return dt;
+           return GetDataTable(new SqlCommand(sqlstatement));
         }
 
         public static void ExecuteSQL(string sqlstatment)
